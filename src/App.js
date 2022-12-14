df146d881df689631a9616edde214ec1e73d313a
Yogesh <yogi.2210@gmail.com>
2022-12-14 22:40:30 +0530

    ternary operator

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@
 import './App.css';
 
 function App() {
+ 
+  const age = 10
+  const isGreen = true
+ 
   return (
     <div className="App">
-      < Job position = "senior SDE" salary = {90000} company="Amazon" />
-      < Job position = "junior SDE" salary ={12000} company="Google" />
-      < Job position = "Project manager" salary = {10000} company="Netflix" />
+     {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>  }
+     <h1 style={{color : isGreen ? "green" : "red" }} >This Text Has Color</h1>
+
+
     </div>
   );
 }
-
-const Job = (props) =>{
-  return (
-    <>
-      <h1> {props.company} </h1>
-      <h1> {props.position} </h1>
-      <h1> {props.salary} </h1>
-    </>
-  )
-}
-
-
 
 
 export default App;
