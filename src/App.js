f7958dc41755e3ba41d4009399ee4de7d8dd11bd
Yogesh <yogi.2210@gmail.com>
2022-12-14 22:21:23 +0530

    create component and pass props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      < Job position = "senior SDE" salary = {90000} company="Amazon" />
+      < Job position = "junior SDE" salary ={12000} company="Google" />
+      < Job position = "Project manager" salary = {10000} company="Netflix" />
     </div>
   );
 }
+
+const Job = (props) =>{
+  return (
+    <>
+      <h1> {props.company} </h1>
+      <h1> {props.position} </h1>
+      <h1> {props.salary} </h1>
+    </>
+  )
+}
+
+
+
 
 export default App;
