50c9225f213921ea0bf7ccd728ada7995fcc5bdb
Yogesh <yogi.2210@gmail.com>
2022-12-15 20:33:52 +0530

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 
 import './App.css';
-import {Planet} from './planet.js'
+import {useState} from "react";
+// import {Planet} from './planet.js'
 
-function App() {
+// function App() {
  
-  const planets = [
-    {name : "Mars", isGasPlanet : false },
-    {name : "Earth", isGasPlanet : false },
-    {name : "Jupiter", isGasPlanet : true},
-    {name : "Venus", isGasPlanet : false },
-    {name : "Neptune", isGasPlanet : true},
-    {name : "Uranus", isGasPlanet : true}
+//   const planets = [
+//     {name : "Mars", isGasPlanet : false },
+//     {name : "Earth", isGasPlanet : false },
+//     {name : "Jupiter", isGasPlanet : true},
+//     {name : "Venus", isGasPlanet : false },
+//     {name : "Neptune", isGasPlanet : true},
+//     {name : "Uranus", isGasPlanet : true}
     
-  ]
+//   ]
   
  
-  return (
-    <div className="App">
-     {planets.map((planet, key)=>{
-        return !planet.isGasPlanet && <Planet name = {planet.name}  />
-     })}
-    </div>
-  );
+//   return (
+//     <div className="App">
+//      {planets.map((planet, key)=>{
+//         return !planet.isGasPlanet && <Planet name = {planet.name}  />
+//      })}
+//     </div>
+//   );
+// }
+function App() {
+  const [inputValue, setInputValue] = useState("");
+
+ const handleInputValue = (e)=>{
+    setInputValue(e.target.value)
+ } 
+
+ return (
+  <div className='App' >
+      <input type="text"  onChange={handleInputValue} />
+       <div>
+        
+        {inputValue}
+        </div> 
+  </div>
+ )
+
 }
 
 
