37499ee73ec94a6c90495b3f1c8be0cff57d41a7
Yogesh <yogi.2210@gmail.com>
2022-12-17 13:18:29 +0530

    implement useEffect and component lifecycle

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
-
 import './App.css';
 import {useState} from "react";
-// import {Planet} from './planet.js'
+import {Text} from "./Text"
 
-// function App() {
- 
-//   const planets = [
-//     {name : "Mars", isGasPlanet : false },
-//     {name : "Earth", isGasPlanet : false },
-//     {name : "Jupiter", isGasPlanet : true},
-//     {name : "Venus", isGasPlanet : false },
-//     {name : "Neptune", isGasPlanet : true},
-//     {name : "Uranus", isGasPlanet : true}
-    
-//   ]
-  
- 
-//   return (
-//     <div className="App">
-//      {planets.map((planet, key)=>{
-//         return !planet.isGasPlanet && <Planet name = {planet.name}  />
-//      })}
-//     </div>
-//   );
-// }
 function App() {
-  const [value, setValue] = useState(0);
-
-  const increaseValue = () =>{
-   setValue(value + 1)
-  } 
-  const decreaseValue = () =>{
-   setValue(value - 1)
-  } 
-  const setToZero = () =>{
-   setValue(0)
-  } 
+  const [showText, setShowText] = useState(false);
 
   return (
-  <div className='App' >
-      <button onClick={increaseValue} >Increase</button>
-      <button onClick={decreaseValue} >Decrease</button>
-      <button onClick={setToZero} >set to zero</button>
-      {value}
-  </div>
+    <div className="App" >
+        <button
+            onClick={()=>{
+                setShowText(!showText)
+            }}
+        >
+            Show Text
+        </button>
+            <div>
+
+        {showText && < Text />}
+            </div>
+
+    </div>
  )
 
 }
